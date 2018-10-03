1c96096f16424ec4b1388a80bd97bb7fa8081ea7
Ty Day <tyrday@gmail.com>
2018-10-03 14:10:40 -0400

    add: base html. /index /menu

--- pizza/urls.py
+++ pizza/urls.py
@@ -13,10 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# copied code from mozillas Django tutorial
+# https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Authentication
+
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path("", include("orders.urls")),
     path("admin/", admin.site.urls),
 ]
+# i dont think i want this
+# path('', RedirectView.as_view(url='/orders')
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+
+#Add Django site authentication urls ( for login, logout, password management)
+urlpatterns += [ path('accounts/', include('django.contrib.auth.urls')),]
+
